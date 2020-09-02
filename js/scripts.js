3095a203f3f53cd1518aeadd1586d9f4c786b18b
Raj Jaisinghani <rajaisinghani6@gmail.com>
2020-09-02 02:44:00 -0400

    good for now need to add info later

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
           $('html, body').animate({
-            scrollTop: (target.offset().top - 71)
+            scrollTop: (target.offset().top - 51)
           }, 1000, "easeInOutExpo");
           return false;
         }
